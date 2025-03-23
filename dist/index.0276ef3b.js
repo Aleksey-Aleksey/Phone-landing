227e807f4f11707b715635610c0e5548bfceaebe
o.ozerov.dev@gmail.com <o.ozerov.dev@gmail.com>
2025-03-23 21:10:08 +0200

    added a dist for publication

---
+++ dist/index.0276ef3b.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",()=>{"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});const e=document.querySelector(".contacts__form");e.addEventListener("submit",t=>{t.preventDefault(),e.reset()});
+//# sourceMappingURL=index.0276ef3b.js.map
